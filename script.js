1d9d462f695ad5dee3830065c2d713be9545ace9
elyk10 <144371327+elyk10@users.noreply.github.com>
2025-08-23 12:56:39 -0400

    created basic functions for calculator

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(x, y)
+{
+    return x + y;
+}
+
+function subtract(x, y)
+{
+    return x - y;
+}
+
+function multiply(x, y)
+{
+    return x * y;
+}
+
+function divide(x, y)
+{
+    return x / y;
+}
+
+
+console.log(add(1, 1));
+console.log(subtract(1, 1));
+console.log(multiply(2, 2));
+console.log(divide(4, 2));
